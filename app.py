1358f109413c450e5734b1f34282b02f6c0eefca
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2023-10-27 11:50:49 -0400

    Update app.py
    Adding functionality for different types of matches. Experimenting with a few different ways we can make it work.
    
    Also began back-end work on maintenance page.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from fuzzywuzzy import fuzz, process
+from bson.objectid import ObjectId
+
+
 
 
 app = Flask(__name__)
@@ -12,6 +16,7 @@ try: # This try operator checks the connection to MongoDB.
 except Exception as error: # Prints an error if the connection fails.
     print(f"MongoDB connection failed: {error}")
 
+
 db = connection['ma_avs_addresses'] # Specifies the name of the database being connected to
 collection = db['ma_avs_addresses'] # Specifies the collection
 
@@ -20,53 +25,139 @@ def run_ui():
     return render_template('user_ui.html')
 
 @app.route('/submitAddress', methods=['POST']) # Defines the root that handles POST requests to the endpoint, which is defined as '/submitAddress'
+
+@app.route('/maintainance_screen')
+def database_management():
+    # Logic to retrieve and display database entries
+    return render_template('maintainance_screen.html')
+
+@app.route('/create_entry', methods=['POST'])
+def create_entry():
+    # Route that allows users to create new entries in the database
+    data = request.form
+    collection.insert_one(data)
+    return redirect(url_for('maintainance_screen'))  # Redirect back to the management page
+
+@app.route('/edit_entry/<entry_id>', methods=['GET', 'POST'])
+def edit_entry(entry_id):
+    # Route that allows users to edit an existing database entry
+    data = request.form
+    filter_criteria = {"_id": ObjectId(entry_id)}
+    update_operation = {
+        "$set": {
+            "addressLine1": data.get("addressLine1"),
+            "addressLine2": data.get("addressLine2"),
+            "city": data.get("city"),
+            "state": data.get("state"),
+            "zip": data.get("zip"),
+            "zipPlusFour": data.get("zipPlusFour"),
+        }
+    }
+
+    collection.update_one(filter_criteria, update_operation)
+    return redirect(url_for('maintainance_screen'))  # Redirect back to the management page
+
+@app.route('/delete_entry/<entry_id>', methods=['POST'])
+def delete_entry(entry_id):
+    # Route that allows users to delete a database entry
+    filter_criteria = {"_id": ObjectId(entry_id)}
+    collection.delete_one(filter_criteria)
+    return redirect(url_for('maintainance_screen'))  # Redirect back to the management page
+
+
+#def fuzziness():
+        #fuzzy_match = fuzz.token_set_ratio()
+
+        #min_threshold = 80
+
+        #return fuzzy_match >= min_threshold
+@app.route('/submitAddress', methods=['POST'])
 def submit_address():
     try:
         # Obtains JSON data from request
         data = request.get_json()
         # Validate and process the data
         entered_addresslineone = data.get('addressLine1') # Gets the selected data out of the JSON data
+        entered_addresslinetwo = data.get('addressLine2') 
         entered_city = data.get('city')
-        entered_state = data.get('stateorprovince') # Gets the selected data out of the JSON data
+        entered_state = data.get('stateorprovince') 
         entered_country = data.get('country')
-        entered_zip = data.get('zip') # Gets the selected data out of the JSON data
-        entered_zipplusfour = data.get('zip +4')       
+        entered_zip = data.get('zip') 
+        entered_zipplusfour = data.get('zipPlusFour')      
+
+        found_address = []
+        match_score = 0
+
         # Query the MongoDB database looking for a match to addressLine1
         found_match = collection.find_one({
              '$or' :[
-            
+            {'addressLine1' : entered_addresslineone,
+             'addressLine2' : entered_addresslinetwo,
+            'city' : entered_city,
+            'stateorprovince' : entered_state,
+            'country' : entered_country,
+            'zip' : entered_zip,
+            'zipPlusFour': entered_zipplusfour        
+                                           },
            { 'addressLine1' : entered_addresslineone,
                                         'city' : entered_city,
                                         'stateorprovince' : entered_state,
-                                           'country' : entered_country,
+                                           'country' : entered_country
                                         },
                                                                                  
                                    
                                   {
-                                        'zip' : entered_zip,
-                                       'zipplusfour' : entered_zipplusfour,}
+                                        'zip' : entered_zip
+                                        }
 ]
    })
 
-        if found_match.get('zip') == entered_zip:
-            zip_match = True
+        #if found_match.get('zip') == entered_zip:
+            #zip_match = True
 
         valid_address = found_match is not None
 
-        address_match = found_match.get('addressLine1') == entered_addresslineone and found_match.get('city') == entered_city and found_match.get('stateorprovince') == entered_state and found_match.get('country') == entered_country
+        address_match = (
+                        found_match.get('addressLine1') == entered_addresslineone and
+                        found_match.get('city') == entered_city and 
+                        found_match.get('stateorprovince') == entered_state and 
+                        found_match.get('country') == entered_country)
 
         zip_match = found_match.get('zip') == entered_zip
 
-        perfect_match = address_match and zip_match
+        partial_match = (zip_match == True or address_match == True) and not (zip_match == True and address_match == True)
 
+        near_match = (
+        found_match.get('addressLine1') == entered_addresslineone and 
+        found_match.get('city') == entered_city and
+        found_match.get('stateorprovince') == entered_state and
+        found_match.get('country') == entered_country and
+        found_match.get('zip') == entered_zip and
+        (found_match.get('addressLine2') != entered_addresslinetwo or found_match.get('zipPlusFour') != entered_zipplusfour))
 
+        perfect_match = (
+        found_match.get('addressLine1') == entered_addresslineone and 
+        found_match.get('city') == entered_city and
+        found_match.get('stateorprovince') == entered_state and
+        found_match.get('country') == entered_country and
+        found_match.get('addressLine2') == entered_addresslinetwo and
+        found_match.get('zip') == entered_zip and
+        found_match.get('zipPlusFour') == entered_zipplusfour)
+# Near match - 5 digit zip code
+# Perfect match - zip code +4, addressLine2 if it's available
 
+# What happens if there is a "perfect match in all fields but zip+4 or adddressLine2?"
 
 
 
                                                         
         # Creates a response
-        response = {'validAddress': valid_address,'matchType':perfect_match, 'message': 'Address is valid; match found.' if valid_address else 'Address is not valid.'} 
+        response = {'validAddress': valid_address,
+                    'perfectMatch': perfect_match,
+                    'nearMatch': near_match, 
+                    'partialMatch': partial_match,
+                    #'matchingAddress': found_match,
+                    'message': 'Address is valid; match found.' if valid_address else 'Address is not valid.'} 
         return jsonify(response), 200
     except Exception as error: # Handles server errors
         return jsonify({f'error': 'Internal server error: {error}'}), 500
