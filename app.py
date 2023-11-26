33963c5402350e45ae19282d956fb4934ddee7ba
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2023-11-26 01:29:31 -0500

    General Debugging/CSV upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fuzzywuzzy import fuzz, process
 from bson.objectid import ObjectId
 from bson.json_util import dumps, loads
 from flask.json import jsonify
+import csv
 
 app = Flask(__name__)
 
@@ -75,32 +76,88 @@ def get_entry(entry_id):
         return jsonify({'error': f'Internal server error: {str(e)}'}), 500
 
 
+def add_addresses():
+    try:
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['file']
+
+        # Check if the file is selected
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Check if the file has the right extension
+        if file and file.filename.endswith('.csv'):
+            # Read CSV file and process entries
+            entries = []
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                entries.append(dict(row))
+
+            # Insert entries into MongoDB collection
+            collection.insert_many(entries)
+
+            return jsonify({'message': f'{len(entries)} entries added successfully'}), 200
+        else:
+            return jsonify({'error': 'Invalid file format. Please upload a .csv file'}), 400
+    except Exception as error:
+        return jsonify({'error': f'Internal server error: {error}'}), 500
+
+
 @app.route('/edit_entry/<entry_id>', methods=['GET', 'POST'])
 def edit_entry(entry_id):
     if request.method == 'GET':
         # Display the edit form with existing entry data
         entry = collection.find_one({"_id": ObjectId(entry_id)})
+
+        # Check each field and replace 'None' with an empty string
+        for key, value in entry.items():
+            if value is None:
+                entry[key] = ''
+
         return render_template('maintenance_screen.html', entry=entry)
     elif request.method == 'POST':
         # Handle the form submission to update the entry
         data = request.form
+
+        # Create a dictionary to hold the non-empty fields
+        update_fields = {}
+
+        # Check each form field and include non-empty ones in the update
+        for field in ['addressLine1', 'addressLine2', 'city', 'stateorprovince', 'zip', 'zipplusfour']:
+            value = data.get(field)
+            if value is not None:
+                update_fields[field] = value
+
         filter_criteria = {"_id": ObjectId(entry_id)}
-        update_operation = {
-            "$set": {
-                "addressLine1": data.get("addressLine1"),
-                "addressLine2": data.get("addressLine2"),
-                "city": data.get("city"),
-                "stateorprovince": data.get("stateorprovince"),
-                "zip": data.get("zip"),
-                "zipplusfour": data.get("zipplusfour"),
-            }
-        }
+        update_operation = {"$set": update_fields}
 
         # Perform the update operation in your MongoDB collection
         collection.update_one(filter_criteria, update_operation)
 
         # Redirect to the list of entries or a success page
         return redirect('/entries')
+
+
+@app.route('/edit_entry/<entryId>', methods=['POST'])
+def edit_existing_entry(entryId):
+    # Extract updated fields from the request form
+    updated_entry = {
+        'addressLine1': request.form.get('addressLine1', ''),
+        'addressLine2': request.form.get('addressLine2', ''),
+        'city': request.form.get('city', ''),
+        'stateorprovince': request.form.get('stateorprovince', ''),
+        'country': request.form.get('country', ''),
+        'zip': request.form.get('zip', ''),
+        'zipplusfour': request.form.get('zipplusfour', ''),
+    }
+
+    # Update the entry in your database using the entryId and updated_entry
+
+    # Redirect to the main page or another appropriate page
+    return redirect('/')
 
 
 @app.route('/delete_entry/<entry_id>', methods=['POST'])
